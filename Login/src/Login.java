0bf80fc9233d287ea9b1b42de07e6f1b69e14d99
ThalesAlbertin <thales.albertin@gmail.com>
2020-08-16 15:02:06 -0300

    Exercicio de Login 16/08/2020

---
+++ Login/src/Login.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class Login {
+
+    public static void main(String[] args) {
+
+        String nome = "";
+        String sobrenome = "";
+        String senha;
+
+        Scanner scanner = new Scanner(System.in);
+
+        boolean i=false;
+        while(!i) {
+
+            System.out.println("Digite seu nome:");
+
+            nome = scanner.nextLine();
+
+            if (nome.matches("[A-Z][a-z]{3,20}")) {
+
+                System.out.println("Ok");
+                i=true;
+
+            } else {
+                System.out.println("Nome inválido!");
+
+            }
+        }
+        i=false;
+        while(!i) {
+
+            System.out.println("Digite seu Sobrenome");
+
+            sobrenome = scanner.nextLine();
+
+            if (sobrenome.matches("[A-Z][a-z]{2,20}")) {
+
+                System.out.println("Ok");
+                i=true;
+
+            } else {
+                System.out.println("Sobrenome inválido!");
+            }
+
+        }
+
+        String login = nome.charAt(0) + "_" + sobrenome;
+
+        System.out.println("Seu login é: " + login);
+
+        i=false;
+        while(!i) {
+
+            System.out.println("Digite uma senha");
+
+            senha = scanner.nextLine();
+            if (senha.matches("^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\\d]){1,})(?=(.*[\\W]){1,})(?!.*\\s).{8,}$")) {
+
+                System.out.println("Usuário "+login+" criado com sucesso!");
+                i=true;
+
+            }else{
+                System.out.println("Senha Inválida!");
+
+            }
+
+        }
+
+    }
+}
